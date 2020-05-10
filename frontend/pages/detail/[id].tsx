2eca4a16106f885276ad31d6a6f861575c6a2004
Elliot <hack00mind@gmail.com>
2020-05-10 22:00:20 +0800

    版权声明

--- frontend/pages/detail/[id].tsx
+++ frontend/pages/detail/[id].tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import MyLayout from "../../components/MyLayout";
+import MyLayout from "../../components/MyLayout"
 import { ClockCircleTwoTone, FireTwoTone } from "@ant-design/icons/lib"
 import marked from 'marked'
 import hljs from 'highlight.js'
-import { Affix } from "antd";
-import Tocify from "../../components/tocify";
+import { Affix, Alert, Divider } from "antd"
+import Tocify from "../../components/tocify"
 import 'highlight.js/styles/monokai-sublime.css'
 import axios from 'axios'
-import {APIRoot} from "../../utils/auth";
-import {GetServerSideProps} from "next";
+import {APIRoot} from "../../utils/auth"
+import {GetServerSideProps} from "next"
+import Link from 'next/link'
 
 
 interface ArticleData {
@@ -25,7 +26,7 @@ interface Props {
 }
 
 const Article = props => {
-  const {series, title, body, views, created} = props.source
+  const {id, series, title, body, views, created} = props.source
   const renderer = new marked.Renderer()
   // @ts-ignore
   renderer.heading = function (text, level, raw) {
@@ -47,92 +48,97 @@ const Article = props => {
 
   return (
     <div>
-      <div>
-        <div className="detail-title">
-          {title}
-        </div>
-
-        <div className="detail-icon center">
-          <span><ClockCircleTwoTone twoToneColor="#ff6666" /> {created.slice(0, 10)}发布</span>
-          <span><FireTwoTone twoToneColor="#ff471a" /> {views}</span>
-        </div>
-
-        <div className="detail-content" dangerouslySetInnerHTML={{__html: markedHtml}}></div>
+      <div className="detail-title">
+        {title}
       </div>
+
+      <div className="detail-icon center">
+        <span><ClockCircleTwoTone twoToneColor="#ff6666" /> {created.slice(0, 10)}发布</span>
+        <span><FireTwoTone twoToneColor="#ff471a" /> {views}</span>
+      </div>
+
+      <div className="detail-content" dangerouslySetInnerHTML={{__html: markedHtml}}></div>
+      <Divider>全文完</Divider>
+      <Alert
+        message="版权声明"
+        description={<Copyright id={id} title={title} />}
+        type="warning"
+        showIcon
+      />
       <style jsx global>{`
-        .detail-title {
-          font-size: 1.8rem;
-          text-align: center;
-          padding: 1rem;
-        }
-        .detail-icon {
-          padding:.5rem 0;
-          color:#AAA;
-        }
-        .detail-icon span {
-          display: inline-block;
-          padding: 0 10px;
-        }
-        .center {
-          text-align: center;
-        }
-        .detail-content {
-          padding: 1.3rem;
-          font-size: 1rem;
-        }
-        pre {
-          display: block;
-          background-color:#f3f3f3;
-          padding: .5rem !important;
-          overflow-y: auto;
-          font-weight: 300;
-          font-family: Menlo, monospace;
-          border-radius: .3rem;
-        }
-        pre {
-          background-color: #283646 !important;
-        }
-        pre >code {
-          border:0px !important;
-          background-color: #283646 !important;
-          color:#FFF;
-        }
-        code {
-          display: inline-block ;
-          background-color:#f3f3f3;
-          border:1px solid #fdb9cc;
-          border-radius:3px;
-          font-size: 14px;
-          padding-left: 5px;
-          padding-right: 5px;
-          color:#4f4f4f;
-          margin: 0px 3px;
-        }
-        .title-anchor {
-          color:#888 !important;
-          padding:4px !important;
-          margin: 0rem !important;
-          height: auto !important;
-          line-height: 1.2rem !important;
-          font-size: .7rem !important;
-          border-bottom: 1px dashed #eee;
-          overflow: hidden;
-          text-overflow:ellipsis;
-          white-space: nowrap;
-        }
-        .active {
-          color:rgb(30, 144, 255) !important;
-        }
-        .nav-title {
-          text-align: center;
-          color: #888;
-          border-bottom: 1px solid rgb(30, 144, 255);
-        }
-        .detail-content img {
-          width: 100%;
-          border:1px solid #f3f3f3;
-        }
-      `}</style>
+      .detail-title {
+        font-size: 1.8rem;
+        text-align: center;
+        padding: 1rem;
+      }
+      .detail-icon {
+        padding:.5rem 0;
+        color:#AAA;
+      }
+      .detail-icon span {
+        display: inline-block;
+        padding: 0 10px;
+      }
+      .center {
+        text-align: center;
+      }
+      .detail-content {
+        padding: 1.3rem;
+        font-size: 1rem;
+      }
+      pre {
+        display: block;
+        background-color:#f3f3f3;
+        padding: .5rem !important;
+        overflow-y: auto;
+        font-weight: 300;
+        font-family: Menlo, monospace;
+        border-radius: .3rem;
+      }
+      pre {
+        background-color: #283646 !important;
+      }
+      pre >code {
+        border:0px !important;
+        background-color: #283646 !important;
+        color:#FFF;
+      }
+      code {
+        display: inline-block ;
+        background-color:#f3f3f3;
+        border:1px solid #fdb9cc;
+        border-radius:3px;
+        font-size: 14px;
+        padding-left: 5px;
+        padding-right: 5px;
+        color:#4f4f4f;
+        margin: 0px 3px;
+      }
+      .title-anchor {
+        color:#888 !important;
+        padding:4px !important;
+        margin: 0rem !important;
+        height: auto !important;
+        line-height: 1.2rem !important;
+        font-size: .7rem !important;
+        border-bottom: 1px dashed #eee;
+        overflow: hidden;
+        text-overflow:ellipsis;
+        white-space: nowrap;
+      }
+      .active {
+        color:rgb(30, 144, 255) !important;
+      }
+      .nav-title {
+        text-align: center;
+        color: #888;
+        border-bottom: 1px solid rgb(30, 144, 255);
+      }
+      .detail-content img {
+        width: 100%;
+        border:1px solid #f3f3f3;
+      }
+    `}</style>
     </div>
   )
 }
@@ -157,6 +163,16 @@ const ArticleNav = ({tocify}) => (
   </Affix>
 )
 
+const Copyright = ({id, title}) => (
+  <div>
+    <span>原文链接：<Link href="/detail/[id]" as={`/detail/${id}`}><a>{title}</a></Link></span>
+    <br />
+    <span>原文作者：<Link href="/about"><a>公子政</a></Link></span>
+    <br />
+    <span>许可协议：<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">署名-非商业性使用-相同方式共享 4.0 国际</a></span>
+  </div>
+)
+
 const Detail = (props: Props) => {
   const tocify = new Tocify()
   const {title} = props.detail
@@ -174,6 +190,7 @@ export const getServerSideProps:GetServerSideProps = async context => {
   const response = await axios.get(APIRoot +
     `articles/${id}/?omit=author,summary,updated,column,tags`)
   const detail = await response.data
+  detail.id = id
   return { props: { detail } }
 }
 
