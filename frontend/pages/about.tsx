908621febed260634aaf98973d23c214348b9565
Elliot <hack00mind@gmail.com>
2020-05-24 18:51:21 +0800

    个人介绍

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -1,12 +1,28 @@
 import MyLayout from "../components/MyLayout"
 import Social from "../components/Social"
+import {Typography} from "antd"
+
+const {Title, Paragraph, Text} = Typography
 
 const About = () => (
-    <MyLayout
-      title="关于-公子政的宅日常"
-      leftContent={<div>hello</div>}
-      rightContent={<Social />}
-    />
+  <MyLayout
+    title="关于-公子政的宅日常"
+    leftContent={
+      <Typography>
+        <Title>关于博客</Title>
+        <Paragraph>
+          <Text strong>前端部分</Text>：<Text mark>NextJs</Text>、<Text mark>Ant Design</Text>
+        </Paragraph>
+        <Paragraph>
+          <Text strong>后端部分</Text>：<Text mark>Django</Text>、<Text mark>Django REST framework</Text>
+        </Paragraph>
+        <Paragraph><Text strong>源码地址</Text>：<a href="https://github.com/Eliot00/elliot00.com">https://github.com/Eliot00/elliot00.com</a></Paragraph>
+        <Title>关于博主</Title>
+        <Paragraph>沉迷于技术海洋中的95后，喜欢尝试新鲜事物，目前在鼓捣<Text mark>deno</Text>。</Paragraph>
+      </Typography>
+    }
+    rightContent={<Social />}
+  />
 )
 
 export default About
