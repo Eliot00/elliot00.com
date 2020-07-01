5ef003044c8a7e7581c22582a04c03a51152452e
Elliot <hack00mind@gmail.com>
2020-07-01 21:06:57 +0800

    友情链接

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -19,6 +19,13 @@ const About = () => (
         <Paragraph><Text strong>源码地址</Text>：<a href="https://github.com/Eliot00/elliot00.com">https://github.com/Eliot00/elliot00.com</a></Paragraph>
         <Title>关于博主</Title>
         <Paragraph>沉迷于技术海洋中的95后，喜欢尝试新鲜事物，目前在鼓捣<Text mark>deno</Text>。</Paragraph>
+        <Title>友情链接</Title>
+        <Paragraph>
+          <ul>
+            <li><a href="https://www.dusaiphoto.com/">杜塞的个人网站</a></li>
+            <li><a href="http://jackypy.xyz/">Jacky的个人网站</a></li>
+          </ul>
+        </Paragraph>
       </Typography>
     }
     rightContent={<Social />}
