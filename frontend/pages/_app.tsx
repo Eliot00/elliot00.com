37092cd598d2d6c5b8b8bfaa7e5b3ef93d179c7e
Elliot <hack00mind@gmail.com>
2020-07-17 14:20:00 +0800

    google analytics

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react'
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
+  return <Component {...pageProps} />
+}
+
+export default App
