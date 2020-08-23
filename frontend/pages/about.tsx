3e9d63fe98d4a5f85e3c6e3e885637b138a8bf99
Elliot <hack00mind@gmail.com>
2020-08-23 14:30:19 +0800

    use h2 title

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -9,7 +9,7 @@ const About = () => (
     title="关于 - 公子政的宅日常"
     leftContent={
       <Typography>
-        <Title>关于博客</Title>
+        <Title level={2}>关于博客</Title>
         <Paragraph>
           <Text strong>前端部分</Text>：<Text mark>NextJs</Text>、<Text mark>Ant Design</Text>
         </Paragraph>
@@ -17,9 +17,9 @@ const About = () => (
           <Text strong>后端部分</Text>：<Text mark>Django</Text>、<Text mark>Django REST framework</Text>
         </Paragraph>
         <Paragraph><Text strong>源码地址</Text>：<a href="https://github.com/Eliot00/elliot00.com">https://github.com/Eliot00/elliot00.com</a></Paragraph>
-        <Title>关于博主</Title>
+        <Title level={2}>关于博主</Title>
         <Paragraph>沉迷于技术海洋中的95后，喜欢尝试新鲜事物，目前在鼓捣<Text mark>deno</Text>。</Paragraph>
-        <Title>友情链接</Title>
+        <Title level={2}>友情链接</Title>
         <Paragraph>
           <ul>
             <li><a href="https://www.dusaiphoto.com/">杜塞的个人网站</a></li>
