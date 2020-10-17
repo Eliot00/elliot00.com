59250777a08133979d786bce29ed6d26d52e392d
Elliot <hack00mind@gmail.com>
2020-10-17 18:22:50 +0800

    test vercel serverless deployment

--- frontend/lib/gtag.js
+++ frontend/lib/gtag.js
@@ -1,4 +1,4 @@
-import { GA_TRACKING_ID } from "../constants"
+const { GA_TRACKING_ID } = process.env
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
