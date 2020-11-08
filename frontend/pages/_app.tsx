7ac035ecf733658e62034effe6bf371895bc793e
Elliot <hack00mind@gmail.com>
2020-11-08 17:11:49 +0800

    feat: delete google analytics

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,23 +2,9 @@ import Header from "../components/Header"
 import { BackTop } from "antd"
 import { Fragment } from "react"
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import * as gtag from '../lib/gtag'
 import Footer from "../components/Footer"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
   return (
     <Fragment>
       <Header />
