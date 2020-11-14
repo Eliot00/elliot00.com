94afb2708663f9b19a2118a90b62b6ea6fd62e9f
Elliot <hack00mind@gmail.com>
2020-11-14 17:27:43 +0800

    feat(view): improved right loading display

--- frontend/components/MyLayout.tsx
+++ frontend/components/MyLayout.tsx
@@ -1,14 +1,14 @@
-import { Row, Col, Skeleton } from 'antd'
+import { Row, Col, Skeleton, Spin } from 'antd'
 import Head from "next/head";
 
-const MyLayout = props => (
+const MyLayout = ({loading, title, leftContent, rightContent}) => (
   <Row className="common-main" justify="center">
-    <Head><title>{props.title}</title></Head>
+    <Head><title>{title}</title></Head>
     <Col className="common-left" xs={24} sm={24} md={16} lg={18} xl={14}>
-      <Skeleton loading={props.loading} active>{props.leftContent}</Skeleton>
+      <Skeleton loading={loading} active>{leftContent}</Skeleton>
     </Col>
     <Col className="common-right" xs={0} sm={0} md={7} lg={5} xl={5}>
-      {props.rightContent}
+      <Spin spinning={loading}>{rightContent}</Spin>
     </Col>
     <style jsx global>{`
       .common-left{
