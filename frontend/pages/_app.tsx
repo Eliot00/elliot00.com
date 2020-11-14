bc94f7fc974c9f39e37f18a230059c580297a36a
Elliot <hack00mind@gmail.com>
2020-11-14 17:42:32 +0800

    chore: update css config with official example

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Fragment, useEffect, useState } from "react"
 import type { AppProps } from 'next/app'
 import Footer from "../components/Footer"
 import { useRouter } from "next/router"
+import 'antd/dist/antd.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(false)
