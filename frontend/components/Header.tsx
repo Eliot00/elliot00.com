783c265d20ca7780163389bb39aadbba776b32e6
Elliot <hack00mind@gmail.com>
2021-03-02 23:07:55 +0800

    chore: rewrite header

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,64 +1,27 @@
 import React from 'react'
 import Link from 'next/link'
-import {Row,Col, Menu} from 'antd'
-import {HomeOutlined, GroupOutlined, InfoCircleOutlined} from '@ant-design/icons'
 
 const Header = () => {
 
   return (
-    <div className="header">
-      <Row justify="center" align="middle">
-        <Col xs={24} sm={24} md={10} lg={15} xl={13}>
-          <span className="header-logo"><Link href="/"><a>公子政</a></Link></span>
-          <span className="header-txt">致虚极&nbsp;守静笃</span>
-        </Col>
-
-        <Col className="menu-div" xs={0} sm={0} md={14} lg={9} xl={6}>
-          <Menu mode="horizontal" defaultSelectedKeys={["home"]}>
-            <Menu.Item key="home">
-              <HomeOutlined/>
-              <Link href="/"><a>首页</a></Link>
-            </Menu.Item>
-            <Menu.Item key="series">
-              <GroupOutlined/>
-              <Link href="/series"><a>归档</a></Link>
-            </Menu.Item>
-            <Menu.Item key="about">
-              <InfoCircleOutlined/>
-              <Link href="/about"><a>关于</a></Link>
-            </Menu.Item>
-          </Menu>
-        </Col>
-      </Row>
-      <style jsx global>{`
-        .header{
-            background-color: #fff;
-            padding: .4rem;
-            overflow: hidden;
-            height: 3.3rem;
-            border-bottom:1px solid #eee;
-        }
-        .header-logo{
-            color:#1e90ff;
-            font-size: 1.6rem;
-            text-align: left;
-        }
-        .header-txt{
-            font-size: 0.6rem;
-            color: #999;
-            display: inline-block;
-            padding-left: 0.3rem;
-        }
-        .ant-menu{
-            line-height: 2.8rem;
-        }
-        .ant-menu-item{
-            font-size:1rem !important;
-            padding-left:1rem;
-            padding-right:1rem;
-        }
-      `}</style>
-    </div>
+    <nav className="mx-auto flex items-center justify-between px-4 sm:px-6 md:px-8">
+      <Link href="/">
+        <a className="flex items-center">
+          <div className="font-sans text-gray-900 text-3xl">公子政</div>
+        </a>
+      </Link>
+      <ul className="flex flex-row px-2 pt-4 pb-3">
+        <li className="pr-5">
+          <Link href="/posts"><a>博客</a></Link>
+        </li>
+        <li className="pr-5">
+          <Link href="/series"><a>归档</a></Link>
+        </li>
+        <li className="pr-5">
+          <Link href="/"><a>关于</a></Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
