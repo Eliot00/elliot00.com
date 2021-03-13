3f7f8edede08bee19a6dece6903cb8a4f6423445
Elliot <hack00mind@gmail.com>
2021-03-13 10:58:00 +0800

    feat: rewrite MyLayout with tailwind grid

--- frontend/components/MyLayout.tsx
+++ frontend/components/MyLayout.tsx
@@ -1,15 +1,15 @@
 import Head from "next/head";
 
 const MyLayout = ({loading, title, leftContent, rightContent}) => (
-  <main className="bg-gray-300">
+  <main className="flex flex-col justify-center mt-2">
     <Head><title>{title}</title></Head>
-    <div className="grid grid-cols-3 gap-3">
-    <div className="bg-white col-span-2 sm:col-span-3">
-      {leftContent}
-    </div>
-    <div className="bg-white col-span-1">
-      {rightContent}
-    </div>
+    <div className="grid grid-cols-12 gap-3">
+      <div className="bg-white col-span-12 md:col-span-8 lg:col-start-2 rounded">
+        {leftContent}
+      </div>
+      <div className="hidden md:block md:col-span-4 lg:col-span-2">
+        {rightContent}
+      </div>
     </div>
   </main>
 )
