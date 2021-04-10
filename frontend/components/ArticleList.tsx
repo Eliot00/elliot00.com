a9b5b284fcb1843ec374812d1510d58d9b09a7fb
Elliot <hack00mind@gmail.com>
2021-04-10 22:15:23 +0800

    feat: fix tag style, increase list spacing

--- frontend/components/ArticleList.tsx
+++ frontend/components/ArticleList.tsx
@@ -9,21 +9,21 @@ const ArticleList = (props) => {
   return (
     <ul className="divide-y divide-gray-100 font-sans">
       {articles.map(article => (
-        <li key={article.slug}>
+        <li key={article.slug} className="py-4">
           <Link href={`/posts/${article.slug}`}>
-            <a className="link hover:underline font-meduim text-2xl">
+            <a className="link font-meduim text-2xl">
               <h2 className="p-2">{article.title}</h2>
             </a>
           </Link>
           <div className="flex">
-            <div className="px-2">{article.column.name}</div>
+            <div className="px-2 text-gray-500">{article.column.name}</div>
             {article.tags.map(tag => (
-              <div className="px-2">{tag.tag.name}</div>
+              <div className="px-2 text-gray-500">{tag.tag.name}</div>
             ))}
-            <time className="px-2" title={article.created}>
+            <time className="px-2 text-gray-500" title={article.created}>
               {timeInterval(article.created)}
             </time>
-            <time className="px-2" title={article.updated}>
+            <time className="px-2 text-gray-500" title={article.updated}>
               {timeInterval(article.updated)}
             </time>
           </div>
