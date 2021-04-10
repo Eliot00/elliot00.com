5fa91ce1621cb2b217ece69dd6357907d7144be7
Elliot <hack00mind@gmail.com>
2021-04-10 20:51:11 +0800

    feat: rewrite copyright component

---
+++ frontend/components/Copyright.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import React from 'react';
+
+interface Props {
+  slug: string,
+  title: string,
+}
+
+const Copyright: React.FC<Props> = ({ slug, title }) => {
+  const selfUrl = `https://www.elliot00.com/posts/${slug}`;
+  return (
+
+    <div className="bg-yellow-50 p-4 rounded flex items-start text-yellow-600 my-4 shadow-md mx-auto">
+      <div className="text-lg">
+        <svg xmlns="http://www.w3.org/2000/svg" className="fill-current w-5 pt-1" viewBox="0 0 24 24"><path d="M12 1l-12 22h24l-12-22zm-1 8h2v7h-2v-7zm1 11.25c-.69 0-1.25-.56-1.25-1.25s.56-1.25 1.25-1.25 1.25.56 1.25 1.25-.56 1.25-1.25 1.25z" /></svg>
+      </div>
+      <div className=" px-3">
+        <div className="text-yellow-800 font-semibold tracking-wider">
+          版权声明
+        </div>
+        <p className="pt-2 text-yellow-700">
+          原文标题：<a href={selfUrl} className="link hover:underline">{title}</a>
+          <br />
+          原文作者：<Link href="/about"><a className="link hover:underline">公子政</a></Link>
+          <br />
+          原文链接：<a href={selfUrl} className="link hover:underline">{selfUrl}</a>
+          <br />
+          许可协议：<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" className="link hover:underline">署名-非商业性使用-相同方式共享 4.0 国际</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Copyright
--- frontend/pages/posts/[slug].tsx
+++ frontend/pages/posts/[slug].tsx
@@ -1,7 +1,6 @@
 import { Alert, Divider } from "antd"
 import { gql, request } from 'graphql-request'
 import { GetStaticPaths, GetStaticProps } from "next"
-import Link from 'next/link'
 import { useRouter } from "next/router"
 import React from 'react'
 import { GraphQLEndpoint } from "../../utils/auth"
@@ -10,6 +9,7 @@ import markdonw from "../../lib/markdown"
 import "prismjs/themes/prism.css"
 import SEO from "../../components/SEO"
 import MarkdownBody from "../../components/MarkdownBody"
+import Copyright from "../../components/Copyright"
 
 const Article = props => {
   const { slug, title, body, created, updated } = props.source
@@ -28,59 +28,9 @@ const Article = props => {
 
         <MarkdownBody content={body} />
         <div className="divide-gray-900 divide-y"><div>EOF</div></div>
-        <Alert
-          message="版权声明"
-          description={<Copyright slug={slug} title={title} />}
-          type="warning"
-          showIcon
-        />
-        <style jsx global>{`
-        .detail-content {
-          padding: 1.3rem;
-          font-size: 1rem;
-        }
-        .title-anchor {
-          color:#888 !important;
-          padding:4px !important;
-          margin: 0rem !important;
-          height: auto !important;
-          line-height: 1.2rem !important;
-          font-size: .7rem !important;
-          border-bottom: 1px dashed #eee;
-          overflow: hidden;
-          text-overflow:ellipsis;
-          white-space: nowrap;
-        }
-        .active {
-          color:rgb(30, 144, 255) !important;
-        }
-        .nav-title {
-          text-align: center;
-          color: #888;
-          border-bottom: 1px solid rgb(30, 144, 255);
-        }
-        .detail-content img {
-          width: 100%;
-          border:1px solid #f3f3f3;
-        }
-      `}</style>
+        <Copyright slug={slug} title={title} />
       </article>
     </>
-  )
-}
-
-const Copyright = ({ slug, title }) => {
-  const selfUrl = `https://www.elliot00.com/posts/${slug}`
-  return (
-    <div>
-      <span>原文标题：<a href={selfUrl} className="link">{title}</a></span>
-      <br />
-      <span>原文作者：<Link href="/about"><a className="link">公子政</a></Link></span>
-      <br />
-      <span>原文链接：<a href={selfUrl} className="link">{selfUrl}</a></span>
-      <br />
-      <span>许可协议：<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" className="link">署名-非商业性使用-相同方式共享 4.0 国际</a></span>
-    </div>
   )
 }
 
