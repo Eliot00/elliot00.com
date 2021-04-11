d9aa08e3f6becb2e8de8eadc7184ca24edc75bd4
Elliot <hack00mind@gmail.com>
2021-04-11 14:21:49 +0800

    feat: rewrite footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,47 +1,12 @@
-const Record = () => (
-  <div className="record">
-    <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41010302002599">
-      <img src="/record.png"/>
-      <p>豫公网安备 41010302002599号</p>
-    </a>
-    <style jsx>{`
-      .record {
-        width: 300px;
-        margin: 0 auto;
-      }
-      a {
-        display: inline-block;
-        text-decoration: none;
-        height: 20px;
-        line-height: 20px;
-      }
-      a > img {
-        float: left;
-      }
-      a > p {
-        float: left;
-        height: 20px;
-        line-height: 20px;
-        margin: 0px 0px 0px 5px;
-        color: #939393;
-      }
-    `}</style>
-  </div>
-)
-
 const Footer = () => (
-  <div className="footer-div">
-    <div>CopyRight © 2020 公子政的宅日常</div>
-    <div>Powered by Django & React <a href="http://www.beian.miit.gov.cn/">豫ICP备20009380号-1</a></div>
-    <Record />
-    <style jsx>{`
-    .footer-div{
-      text-align: center;
-      width: 100%;
-      padding: 1rem;
-      color:#888;
-    }
-  `}</style>
+  <div className="text-center p-4 text-sm">
+    <div className="text-gray-600">Copyright © 2020-2021 公子政的宅日常</div>
+    <div className="inline-flex">
+      <a href="http://www.beian.miit.gov.cn/" className="link">豫ICP备20009380号-1</a>
+      <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41010302002599" className="link inline-flex">
+        <img src="/record.png"/>豫公网安备 41010302002599号
+      </a>
+    </div>
   </div>
 )
 
