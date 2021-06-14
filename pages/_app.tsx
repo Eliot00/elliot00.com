b5f70bbc29367b7f27b07795a0024ceeaff7b850
Elliot <hack00mind@gmail.com>
2021-06-14 19:13:38 +0800

    chore: remove loading in custom _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,35 +5,11 @@ import MyLayout from "../components/MyLayout"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(false)
   const router = useRouter()
-
-  const startLoading = () => {
-    console.log('route change start')
-    setLoading(true)
-  }
-
-  const stopLoading = () => {
-    console.log('route change complete')
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    router.events.on('routeChangeStart', startLoading)
-    router.events.on('routeChangeComplete', stopLoading)
-    router.events.on('routeChangeError', stopLoading)
-
-    return () => {
-      router.events.off('routeChangeStart', startLoading)
-      router.events.off('routeChangeComplete', stopLoading)
-      router.events.on('routeChangeError', stopLoading)
-    }
-
-  }, [])
 
   return (
     <MyLayout>
-      <Component loading={loading} {...pageProps} />
+      <Component {...pageProps} />
     </MyLayout>
   )
 }
