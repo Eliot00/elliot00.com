c9f9acdaf42a905578894e405098f4f420a5e61b
Elliot <hack00mind@gmail.com>
2021-08-04 21:51:35 +0800

    fix: dependence

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,15 +1,12 @@
 import { FunctionComponent } from "react"
-import MarkdownIt from "markdown-it"
 
 type Props = {
     raw: string
 }
 
-const summaryMd = MarkdownIt()
 
 const Summary: FunctionComponent<Props> = ({ raw }) => {
-    const rendered = summaryMd.renderInline(raw)
-    return <p className="p-2 font-serif font-thin text-gray-500" dangerouslySetInnerHTML={ { __html: rendered } }></p>
+    return <p className="p-2 font-serif font-thin text-gray-500" dangerouslySetInnerHTML={ { __html: raw } }></p>
 }
 
 export default Summary
