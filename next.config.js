e052a327e20a6844c790efa48461c310295bbb33
Elliot <hack00mind@gmail.com>
2021-11-13 18:57:11 +0800

    chore(dependencies): upgrade nextjs to version 12

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer()
+module.exports = {
+  swcMinify: true,
+}
