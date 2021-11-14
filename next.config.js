8b89bf58b3c9493b871a1161dc4fcf7e211f6738
Elliot <hack00mind@gmail.com>
2021-11-14 20:17:11 +0800

    chore: test load mdx

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   swcMinify: true,
-}
+})
