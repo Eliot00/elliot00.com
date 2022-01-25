d04dfd6635f46e3df7956a0f3e563b79f3da0b52
Elliot <hack00mind@gmail.com>
2022-01-25 13:13:36 +0800

    wip: try mdx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import type { AppProps } from 'next/app'
 import { useRouter } from "next/router"
 import MyLayout from "../components/MyLayout"
 import * as gtag from "../lib/gtag"
-import { MDXProvider } from "@mdx-js/react"
-import components from "../components/typography"
 import '../styles/globals.css'
 
 
@@ -21,11 +19,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [router.events])
 
   return (
-    <MDXProvider components={components}>
-      <MyLayout>
-        <Component {...pageProps} />
-      </MyLayout>
-    </MDXProvider>
+    <MyLayout>
+      <Component {...pageProps} />
+    </MyLayout>
   )
 }
 
