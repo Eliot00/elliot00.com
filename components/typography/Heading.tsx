531f0aa5d83b2f0d91b4ca02daa14b0cfb10de8a
Elliot <hack00mind@gmail.com>
2022-04-10 16:08:57 +0800

    chore: update heading border style

--- components/typography/Heading.tsx
+++ components/typography/Heading.tsx
@@ -2,25 +2,26 @@ import React from 'react';
 
 export const Heading2: React.FC = props => (
   <h2
-    className="text-2xl leading-tight mt-6 mb-4 font-semibold font-serif border-l-4 pl-2 border-indigo-200"
+    className="text-2xl leading-tight mt-6 mb-4 font-semibold font-serif border-b-2 pb-1 border-indigo-300 inline-block"
     {...props}
   />
 )
 
 export const Heading3: React.FC = props => (
   <h3
-    className="text-lg leading-snug mt-6 mb-4 font-semibold font-serif"
+    className="text-lg leading-snug mt-6 mb-4 font-semibold font-serif border-l-4 pl-2 border-indigo-200"
     {...props}
   />
 )
 
 export const Heading4: React.FC = props => (
   <h4
-    className="leading-none text-base font-semibold mb-4 mt-6"
+    className="leading-none text-base font-semibold mb-4 mt-6 text-indigo-300"
     {...props}
   />
 )
 
+// variant 5 and 6 is less commonly used
 export const Heading5: React.FC = props => (
   <h5
     className="leading-tight text-sm font-semibold mb-4 mt-6"
