66826d459ddd176504d93f1cce5e51ebb15c6cb4
Elliot <hack00mind@gmail.com>
2022-04-23 18:33:54 +0800

    feat: a simple way to get first paragraph of mdx source

--- lib/getFirstParagraph.ts
+++ lib/getFirstParagraph.ts
@@ -1,13 +1,21 @@
-/* eslint-disable */
-import marked from 'marked'
-
 const YAMLFrontMatter = /---(.|\n)*---/
+const imageRegex = /(\!\[[^\]]+\]\([^\)]+\))/g
+
 export default function getFirstParagraph(raw: string): string {
-  const tokens = (marked as any).lexer(raw.replace(YAMLFrontMatter, ''))
-  for (const token of tokens) {
-    if (token.type === 'paragraph') {
-      return token.text
+  const mdString = raw
+    .trim()
+    .replace(YAMLFrontMatter, '')
+    .replace(imageRegex, '')
+
+  const lines = mdString.split('\n')
+  for (const line of lines) {
+    if (!line.startsWith('#') && line.length > 20) {
+      const pureString = line.replaceAll('*', '').replaceAll('`', '')
+      if (pureString.length > 150) {
+        return `${pureString.substring(0, 150)}...`
+      }
+      return pureString
     }
   }
-  return raw
+  return mdString
 }
