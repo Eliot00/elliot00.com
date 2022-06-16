e56b28558f6f541199d2f0e9d8e1e55b19c88995
Elliot <hack00mind@gmail.com>
2022-06-16 22:52:19 +0800

    feat: optimize Chinese typesettion

--- components/typography/Blockquote.tsx
+++
@@ -1,5 +0,0 @@
-const Blockquote: React.FC = props => (
-    <blockquote className="text-base border-l-4 border-gray-300 px-4 my-2 text-gray-600" {...props} />
-)
-
-export default Blockquote
--- components/typography/index.tsx
+++ components/typography/index.tsx
@@ -1,6 +1,5 @@
 import CustomLink from './CustomLink'
 import { Heading2, Heading3, Heading4, Heading5, Heading6 } from './Heading'
-import Blockquote from './Blockquote'
 import Code from './Code'
 
 const components = {
@@ -10,7 +9,6 @@ const components = {
   h5: Heading5,
   h6: Heading6,
   a: CustomLink,
-  blockquote: Blockquote,
   code: Code,
 }
 
