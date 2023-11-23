edcd1380f7f9be3766f91bfae57a1361460baaf6
Elliot <hack00mind@gmail.com>
2023-11-23 15:59:48 +0800

    chore: declare html language property

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,33 +3,33 @@ import MyLayout from '@/components/MyLayout'
 import './globals.css'
 
 export const metadata: Metadata = {
-    title: 'Elliot',
-    description: 'Elliot\'s blog.'
+  title: 'Elliot',
+  description: "Elliot's blog.",
 }
 
 export default function RootLayout({
-    // Layouts must accept a children prop.
-    // This will be populated with nested layouts or pages
-    children,
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
-    // const router = useRouter()
-    // useEffect(() => {
-    //     const handleRouteChange = (url: string) => {
-    //         gtag.pageview(url)
-    //     }
-    //     router.events.on('routeChangeComplete', handleRouteChange)
-    //     router.events.on('hashChangeComplete', handleRouteChange)
-    //     return () => {
-    //         router.events.off('routeChangeComplete', handleRouteChange)
-    //         router.events.off('hashChangeComplete', handleRouteChange)
-    //     }
-    // }, [router.events])
+  // const router = useRouter()
+  // useEffect(() => {
+  //     const handleRouteChange = (url: string) => {
+  //         gtag.pageview(url)
+  //     }
+  //     router.events.on('routeChangeComplete', handleRouteChange)
+  //     router.events.on('hashChangeComplete', handleRouteChange)
+  //     return () => {
+  //         router.events.off('routeChangeComplete', handleRouteChange)
+  //         router.events.off('hashChangeComplete', handleRouteChange)
+  //     }
+  // }, [router.events])
 
-    return (
-        <html lang="en">
-            {/*
+  return (
+    <html lang="zh-CN">
+      {/*
             <head>
                 <script
                     dangerouslySetInnerHTML={{
@@ -50,7 +50,9 @@ export default function RootLayout({
                 src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
             />
               */}
-            <body><MyLayout>{children}</MyLayout></body>
-        </html>
-    )
+      <body>
+        <MyLayout>{children}</MyLayout>
+      </body>
+    </html>
+  )
 }
