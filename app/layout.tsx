46296a2cea2a16ed52cfd2ad710791d8dedcde78
Elliot <hack00mind@gmail.com>
2023-11-23 16:13:31 +0800

    fix: gtag with next 14

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next'
 import MyLayout from '@/components/MyLayout'
 import './globals.css'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: 'Elliot',
@@ -14,44 +15,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  // const router = useRouter()
-  // useEffect(() => {
-  //     const handleRouteChange = (url: string) => {
-  //         gtag.pageview(url)
-  //     }
-  //     router.events.on('routeChangeComplete', handleRouteChange)
-  //     router.events.on('hashChangeComplete', handleRouteChange)
-  //     return () => {
-  //         router.events.off('routeChangeComplete', handleRouteChange)
-  //         router.events.off('hashChangeComplete', handleRouteChange)
-  //     }
-  // }, [router.events])
-
   return (
     <html lang="zh-CN">
-      {/*
-            <head>
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-
-                            gtag('config', '${gtag.GA_TRACKING_ID}', {
-                                page_path: window.location.pathname,
-                            });
-                        `,
-                    }}
-                />
-            </head>
-            <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-            />
-              */}
       <body>
         <MyLayout>{children}</MyLayout>
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GA_ID} />
       </body>
     </html>
   )
