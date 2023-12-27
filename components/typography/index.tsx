b15845e34427fbf15cd854a7aee6dfa6cab7c26c
Elliot <hack00mind@gmail.com>
2023-12-27 14:30:10 +0800

    fix: mdx type missing

--- components/typography/index.tsx
+++ components/typography/index.tsx
@@ -1,10 +1,9 @@
-import { type MDXComponents } from 'mdx/types'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 // @ts-ignore
 import { oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
-const components: MDXComponents = {
-  code: ({ className, ...props }) => {
+const components: any = {
+  code: ({ className, ...props }: any) => {
     if (!className) {
       return <code className="bg-slate-200 px-2 rounded" {...props} />
     }
@@ -15,7 +14,7 @@ const components: MDXComponents = {
       <SyntaxHighlighter language={match[1]} style={oneLight} {...props} />
     )
   },
-  a: ({ children, href }) => (
+  a: ({ children, href }: any) => (
     <a
       href={href}
       className="text-indigo-500 hover:text-indigo-900 underline transition duration-150 ease-in-out"
