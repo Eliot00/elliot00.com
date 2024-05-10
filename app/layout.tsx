6a6b7c82f6d19c1bbb177423e9aea8588ccd878e
Elliot <hack00mind@gmail.com>
2024-05-10 15:45:01 +0800

    chore: pin footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-CN">
-      <body>
+      <body className="flex flex-col min-h-screen">
         <Header />
-        <main className="max-w-3xl mx-auto p-4 sm:px-6 md:px-8 lg:px-0">
+        <main className="flex-1 max-w-3xl mx-auto p-4 sm:px-6 md:px-8 lg:px-0">
           {children}
         </main>
         <Footer />
