1ab361f77755ef652dcc8ccda85e59de3f31b805
Elliot <hack00mind@gmail.com>
2024-06-25 16:03:20 +0800

    feat: update social icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,16 @@
-import { GithubIcon, TwitterIcon } from '@/components/icons'
+import { GithubIcon, MastodonIcon } from '@/components/icons'
+
+const ICON_CLASS =
+  'inline-block text-gray-600 transition ease-in-out hover:text-purple-600 hover:rotate-6 hover:scale-110'
 
 const Footer = () => (
   <div className="text-center p-4 text-sm">
     <div className="space-x-2 mb-4">
-      <a className="link" href="https://github.com/Eliot00">
+      <a className={ICON_CLASS} href="https://github.com/Eliot00">
         <GithubIcon />
       </a>
-      <a className="link" href="https://twitter.com/CoderElliot">
-        <TwitterIcon />
+      <a className={ICON_CLASS} href="https://mastodon.social/@CoderElliot">
+        <MastodonIcon />
       </a>
     </div>
     <div className="text-gray-600">Copyright © 2020-2024 Elliot</div>
