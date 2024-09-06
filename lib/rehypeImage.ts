765a308a461e0f1bcc1cbaf45aa1a670252d2bea
Elliot <hack00mind@gmail.com>
2024-09-06 09:53:38 +0800

    feat: add caching mechanism to optimize image probe

--- lib/rehypeImage.ts
+++ lib/rehypeImage.ts
@@ -1,6 +1,19 @@
 import { visit } from 'unist-util-visit'
 // @ts-ignore
 import probe from 'probe-image-size'
+import path from 'node:path'
+import fs from 'node:fs/promises'
+
+const CACHE_FILE = path.join(process.cwd(), 'image-size-cache.json')
+
+interface ImageSize {
+  width: number
+  height: number
+}
+
+interface Cache {
+  [src: string]: ImageSize
+}
 
 const anyVisit = visit as any
 
@@ -31,8 +44,36 @@ export default function rehypeProbeImageSize() {
 }
 
 async function setImageSize(src: string, node: any) {
-  const result = await probe(src)
+  const result = await probeCache(src)
 
   node.properties.width = result.width
   node.properties.height = result.height
+}
+
+async function probeCache(src: string): Promise<ImageSize> {
+  const cache = await readCache()
+
+  if (cache[src]) {
+    return cache[src]
+  }
+
+  const result = await probe(src)
+  cache[src] = { width: result.width, height: result.height }
+
+  await writeCache(cache)
+
+  return cache[src]
+}
+
+async function readCache(): Promise<Cache> {
+  try {
+    const data = await fs.readFile(CACHE_FILE, 'utf-8')
+    return JSON.parse(data)
+  } catch (error) {
+    return {}
+  }
+}
+
+async function writeCache(cache: Cache): Promise<void> {
+  await fs.writeFile(CACHE_FILE, JSON.stringify(cache, null, 2))
 }
