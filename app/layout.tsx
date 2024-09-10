308e270b155adaf96d4477f474c186735b3f16e2
Elliot <hack00mind@gmail.com>
2024-09-10 10:20:18 +0800

    feat: prepare google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import AdSense from '@/components/AdSense'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -19,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-CN">
+      <head>
+        <AdSense pid="4587152222007322" />
+      </head>
       <body className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-1 w-full max-w-3xl self-center p-4 sm:px-6 md:px-8 lg:px-0">
---
+++ components/AdSense.tsx
@@ -0,0 +1,20 @@
+import Script from 'next/script'
+
+type AdSenseProps = {
+  pid: string
+}
+
+export default function AdSense({ pid }: AdSenseProps) {
+  if (process.env.NODE_ENV !== 'production') {
+    return null
+  }
+
+  return (
+    <Script
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pid}`}
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+      async
+    />
+  )
+}
