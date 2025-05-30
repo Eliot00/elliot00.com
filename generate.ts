0710980c6a555b94c611fbbf33fda449c3bf1c59
Elliot <hack00mind@gmail.com>
2025-05-30 14:26:32 +0800

    chore: update docube

--- generate.ts
+++ generate.ts
@@ -1,5 +1,6 @@
 import { ContentConverter, Unified } from 'docube'
 import { makeMarkdownConverter } from '@docube/markdown'
+import { makeUnifiedLive } from '@docube/org'
 import {
   makeTransformer,
   makeAppConfig,
@@ -14,11 +15,6 @@ import { Effect, Layer } from 'effect'
 import rehypeShiftHeading from 'rehype-shift-heading'
 import rehypePrettyCode from 'rehype-pretty-code'
 import slug from 'rehype-slug-custom-id'
-import parse from 'uniorg-parse'
-import uniorg2rehype from 'uniorg-rehype'
-import stringify from 'rehype-stringify'
-import extractKeywords from 'uniorg-extract-keywords'
-import { unified } from 'unified'
 import raw from 'rehype-raw'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeMathjax from 'rehype-mathjax'
@@ -52,29 +48,18 @@ const rehypePrettyOptions = {
   transformers: [copyButtonSlotTransformer()],
 }
 
-const orgProcessor = unified()
-  .use(parse)
-  .use(extractKeywords)
-  .use(uniorg2rehype)
-  .use(raw)
-  .use(rehypeProbeImageSize)
-  .use(rehypeShiftHeading, { shift: 1 })
-  // @ts-expect-error unknown cache error
-  .use(rehypePrettyCode, rehypePrettyOptions)
-  .use(slug)
-  .use(rehypeAutolinkHeadings, { behavior: 'wrap' })
-  .use(rehypeMathjax)
-  .use(rehypeCallouts)
-  .use(stringify)
-
-const UnifiedLive = Layer.succeed(
-  Unified,
-  Unified.of({
-    process(content) {
-      return Effect.promise(() => orgProcessor.process(content))
-    },
-  })
-)
+const UnifiedLive = makeUnifiedLive({
+  rehypePlugins: [
+    raw,
+    rehypeProbeImageSize,
+    [rehypeShiftHeading, { shift: 1 }],
+    [rehypePrettyCode, rehypePrettyOptions],
+    slug,
+    [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+    rehypeMathjax,
+    rehypeCallouts,
+  ],
+})
 
 const MarkdownConverterLive = makeMarkdownConverter({
   remarkPlugins: [remarkGfm],
