27f9a04f7d9a2b4dfdd90f0e5a6bdd69e686d61f
Elliot <hack00mind@gmail.com>
2025-09-26 15:06:53 +0800

    feat: set smooth scroll behavior

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="zh-CN" className={`${lxgw.variable} antialiased`}>
+    <html lang="zh-CN" className={`${lxgw.variable} antialiased scroll-smooth`}>
       <head>
         <AdSense pid="4587152222007322" />
       </head>
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const COMMON_PROSE_CLASS_NAME =
-  'prose prose-sm sm:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert mx-auto'
+  'prose prose-sm sm:prose-base lg:prose-lg dark:prose-invert mx-auto'
 
 export const COMMON_LINK_CLASS_NAME =
   'underline decoration-foreground/20 hover:no-underline transition-all duration-500 ease-in-out bg-bottom-left bg-linear-to-r from-primary to-primary bg-size-[0%_1.5px] bg-no-repeat hover:bg-size-[100%_1.5px]'
